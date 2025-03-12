8d4e033a726df4ff6323cbdaafed628fd859fecf
mohammed-alhashim <m.alhashim99@hotmail.com>
2025-03-13 00:53:07 +0300

    Third day fixed all the csv

--- project/bank_app.py
+++ project/bank_app.py
@@ -163,3 +163,95 @@ class Withdraw(Account):
 
         return f"Withdrawal successful. New balance: {current_user['balance_savings']}"
 
+class Deposit(Account):
+    rows = []
+
+    def __init__(self, account):
+        global accounts
+        with open('transactions.csv', mode='r', newline='') as read_file:
+            reader = csv.DictReader(read_file)
+            for row in reader:
+                self.rows.append(row)
+
+    def overdraft(self):
+        global current_user
+        with open('transactions.csv', mode='r', newline='') as read_file:
+            reader = csv.DictReader(read_file)
+            rows = []
+            for row in reader:
+                if row['account_id'] == current_user['account_id'] and row['overdraft'] == "True":
+                    rows.append(row)
+
+        if len(rows) > 1:
+            return True
+
+    def deposit_money(self, amount):
+        global accounts
+        overdraft = True
+        if not self.login(current_user['account_id'], current_user['password']):
+            return "Please log in first"
+        if amount > 0:
+            current_user["balance_checking"] = float(current_user['balance_checking']) + amount
+        if self.overdraft():
+            if float(current_user['balance_checking']) >= 0:
+                current_user['active'] = True
+
+        list_of_do = {
+            "account_id": current_user['account_id'], "amount": amount, "type": 'balance_checking',
+            "overdraft": overdraft}
+        with open('transactions.csv', 'a', newline='') as csvfile:
+            fieldnames = ["account_id", "amount", "type", "overdraft"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            # writer.writeheader()
+            writer.writerow(list_of_do)
+        list_of_values = {
+            "account_id": current_user['account_id'], "first_name": current_user['first_name'],
+            "last_name": current_user['last_name'],
+            "password": current_user['password'], "balance_checking": float(current_user['balance_checking']),
+            "balance_savings": float(current_user['balance_savings']), "active": current_user['active']}
+
+        with open('bank.csv', 'w', newline='') as csvfile:
+            fieldnames = ["account_id", "first_name", "last_name", "password", "balance_checking", "balance_savings",
+                          "active"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow(list_of_values)
+
+        return f"Deposit successful. New balance: {current_user['balance_checking']}"
+
+    def deposit_in_saving(self, amount):
+        global accounts
+        overdraft = True
+        if not self.login(current_user['account_id'], current_user['password']):
+            return "Please log in first"
+        if amount > 0:
+            current_user["balance_savings"] = float(current_user['balance_savings']) + amount
+        if self.overdraft():
+            if float(current_user['balance_savings']) >= 0:
+                current_user['active'] = True
+
+        list_of_do = {
+            "account_id": current_user['account_id'], "amount": amount, "type": 'balance_savings',
+            "overdraft": overdraft}
+
+        with open('transactions.csv', 'a', newline='') as csvfile:
+            fieldnames = ["account_id", "amount", "type", "overdraft"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            # writer.writeheader()
+            writer.writerow(list_of_do)
+        list_of_values = {
+            "account_id": current_user['account_id'], "first_name": current_user['first_name'],
+            "last_name": current_user['last_name'],
+            "password": current_user['password'], "balance_checking": float(current_user['balance_checking']),
+            "balance_savings": float(current_user['balance_savings']), "active": current_user['active']}
+
+        with open('bank.csv', 'w', newline='') as csvfile:
+            fieldnames = ["account_id", "first_name", "last_name", "password", "balance_checking", "balance_savings",
+                          "active"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow(list_of_values)
+
+        return f"Deposit successful. New balance: {current_user['balance_savings']}"
+
+
